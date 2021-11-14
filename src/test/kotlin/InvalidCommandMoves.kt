3b14938055e4597f44900bc6b55f64619a86bcac
Henriquess19 <80883225+Henriquess19@users.noreply.github.com>
2021-11-14 13:32:31 +0000

    tests ig

--- src/test/kotlin/InvalidCommandMoves.kt
+++ src/test/kotlin/InvalidCommandMoves.kt
@@ -5,18 +5,21 @@ import toMove
 import kotlin.test.assertEquals
 
 class InvalidCommandMoves {
+
     @Test(expected = IllegalArgumentException::class)
     fun `move empty`() {
         val sut = Board()
         sut
             .makeMove("".toMove(),  teamTurn(sut.getMoveList(),null))
     }
+
     @Test(expected = IllegalArgumentException::class)
     fun `move with only one character`() {
         val sut = Board()
         sut
             .makeMove("p".toMove(),  teamTurn(sut.getMoveList(),null))
     }
+
     @Test(expected = IllegalArgumentException::class)
     fun `move command to big`() {
         val sut = Board()
